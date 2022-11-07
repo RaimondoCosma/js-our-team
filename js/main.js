7f2e1566dd5334c17c1991bba071d0a12d3ecc87
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-07 13:22:30 +0100

    Crea oggetto con i membri del team

---
+++ js/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Dichiaro variabile relativa all'oggetto Team
+const teamMembers = [
+    {
+        fullName: 'Wayne Barnett',
+        occupation: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        fullName: 'Angela Caroll',
+        occupation: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        fullName: 'Walter Gordon',
+        occupation: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        fullName: 'Angela Lopez',
+        occupation: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        fullName: 'Scott Estrada',
+        occupation: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    },
+    {
+        fullName: 'Barbara Ramos',
+        occupation: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    },
+]
+
+console.log(teamMembers);
